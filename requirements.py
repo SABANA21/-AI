8503b82fb236c13e4df80912434c0915c29b82ab
SABANA21 <zsabana21@gmail.com>
2026-03-27 12:35:55 +0530

    requirements added

--- requirements.py
+++ requirements.py
@@ -1,1 +1,10 @@
-import streamlit as st
+langchain==1.2.10
+python-dotenv==1.2.1
+streamlit==1.28.2
+unstructured==0.20.8
+tiktoken==0.12.0
+faiss-cpu==1.13.2
+
+python-magic==0.4.27
+python-magic-bin==0.4.14
+OpeNAI==2.21.0
